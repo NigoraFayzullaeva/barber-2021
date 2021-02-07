27c28fbdc52f8dde4b041fe9030a62e6310177f2
Nigora <61627731+NigoraFayzullaeva@users.noreply.github.com>
2021-02-07 20:40:01 +0300

    gallery added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
+// AOS init
 AOS.init();
+
+//Slick slider
+$('.gallery').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+            }
+        },
+        {
+            breakpoint: 450,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+
+    ]
+});
